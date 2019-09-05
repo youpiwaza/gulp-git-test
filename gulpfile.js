3975b55489271354998a3499baed3eb0a5a954c7
Max <masamune.code@gmail.com>
2019-09-05 17:51:27 +0200

    MOD: Refacto & comments

--- gulpfile.js
+++ gulpfile.js
@@ -10,29 +10,30 @@ const del = require('del');
 const fancyLog = require('fancy-log');
 const git = require('gulp-git');
 
+
 // // Tasks
-
-
-
-// Clone remote repo to sub folder ($CWD/sub/folder/git-test)
-task('clone-mine', function() {
-  git.clone('https://github.com/youpiwaza/chaos-toolbox', {args: './clonehere/sub'}, function(status) {
-
-    if(status === undefined) {
-      fancyLog(colors.green('Repo cloné avec succès'));
-      return true;
-    }
-    else {
-      fancyLog(colors.red('Error lel'));
-      console.log(status);
-      return false;
-    }
-  });
-});
-
-task('clean', function(){
+// Delete cloned repo folder content
+task('clean', () => {
   return del('clonehere/**', {force:true});
 });
 
+// Clone my repo where it's needed
+task('clone-my-repo', async () => {
+  // Clone remote repo to sub folder ($CWD/clonehere/sub)
+  git.clone('https://github.com/youpiwaza/chaos-toolbox', {args: './clonehere/sub'}, async (status) => {
+
+    if(status === undefined) {
+      fancyLog(colors.green('Repo cloné avec succès'));
+      await Promise.resolve('Repo cloné avec succès');
+    }
+    else {
+      fancyLog(colors.red('Error lel'));
+      fancyLog(status);
+      await Promise.resolve('Error lel');
+    }
+  })
+});
+
 // Default tesk, executed when using 'gulp'
-task('default', series('clean', 'clone-mine'));
+//  Clean then clone
+task('default', series('clean', 'clone-my-repo'));
