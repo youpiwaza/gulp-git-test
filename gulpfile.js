56b1b8cc3ea04d929250ae3b60117a99294a9125
Max <masamune.code@gmail.com>
2019-09-05 17:31:42 +0200

    ADD: git clone test & del folder

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,30 @@ const {
   task,
 } = require('gulp');
 
+const colors = require('ansi-colors');
+const del = require('del');
+const fancyLog = require('fancy-log');
 const git = require('gulp-git');
 
 // // Tasks
 
 // Default tesk, executed when using 'gulp'
-// task('default', parallel('copy-assets'));
+// Clone remote repo to sub folder ($CWD/sub/folder/git-test)
+task('default', function() {
+  git.clone('https://github.com/youpiwaza/chaos-toolbox', {args: './clonehere/sub'}, function(status) {
+
+    if(status === undefined) {
+      fancyLog(colors.green('Repo cloné avec succès'));
+      return true;
+    }
+    else {
+      fancyLog(colors.red('Error lel'));
+      console.log(status);
+      return false;
+    }
+  });
+});
+
+task('clean', function(){
+  return del('clonehere/**', {force:true});
+});
