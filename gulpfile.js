dbef4963b325615f8b882e02573f0a6caba0c41c
Max <masamune.code@gmail.com>
2019-09-05 18:49:02 +0200

    ADD: Warning for current directory open, preventing correct permissions

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,6 @@
+//// config
+const pathWhereToClone = './clonehere/sub';
+
 // // Resources
 // Plugins
 const {
@@ -13,6 +16,7 @@ const fancyLog = require('fancy-log');
 const git = require('gulp-git');
 const yarn = require('gulp-yarn');
 
+const spawn = require('child_process').spawn;
 
 // // Tasks
 // Delete cloned repo folder content
@@ -24,7 +28,7 @@ task('clean', () => {
 task('clone-my-repo', (done) => {
   const repoUri = 'https://github.com/youpiwaza/chaos-toolbox';
   // Clone remote repo to sub folder ($CWD/clonehere/sub)
-  git.clone(repoUri, {args: './clonehere/sub'}, (status) => {
+  git.clone(repoUri, {args: pathWhereToClone}, (status) => {
 
     if(status === undefined) {
       fancyLog(c.green(`Repo '${c.cyan.italic(repoUri)}' cloné avec succès`));
@@ -42,7 +46,7 @@ task('clone-my-repo', (done) => {
 task('clone-my-private-repo', (done) => {
   const repoUri = 'https://github.com/youpiwaza/boilerplate-exercice';
   // Clone remote repo to sub folder ($CWD/clonehere/sub)
-  git.clone(repoUri, {args: './clonehere/sub'}, (status) => {
+  git.clone(repoUri, {args: pathWhereToClone}, (status) => {
 
     if(status === undefined) {
       fancyLog(c.green(`Repo '${c.cyan.italic(repoUri)}' cloné avec succès`));
@@ -61,19 +65,25 @@ task('clone-my-private-repo', (done) => {
 //     done();
 // });
 
+// Warn user
+task('warn', (done) => {
+    fancyLog(c.yellow(`Attention, il ne faut pas laisser le dossier où le clone sera ajouté ouvert (NI explorateur NI IDE) : ${pathWhereToClone}`));
+    done();
+});
+
 task('yarn', () => {
     //                                                                        if not present, create
-    return src(['./clonehere/sub/package.json', './clonehere/sub/yarn.lock'], { allowEmpty: true })
-        .pipe(dest('./clonehere/sub'))
+    return src([`${pathWhereToClone}/package.json`, `${pathWhereToClone}/yarn.lock`], { allowEmpty: true })
+        .pipe(dest(pathWhereToClone))
         .pipe(yarn());
 });
 
 // Default tesk, executed when using 'gulp'
 //  Clean then clone
-task('default', series('clean', 'clone-my-repo'));
-task('get-private-repo', series('clean', 'clone-my-private-repo'));
+task('default', series('warn', 'clean', 'clone-my-repo'));
+task('get-private-repo', series('warn', 'clean', 'clone-my-private-repo'));
 // task('clone-n-install', series('clean', 'clone-my-private-repo', 'log' ));
-task('clone-n-install', series('clean', 'clone-my-private-repo', 'yarn'));
+task('clone-n-install', series('warn', 'clean', 'clone-my-private-repo', 'yarn'));
 
 
 
