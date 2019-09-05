551e8f7b7b747bed844e61a2bd191cdbf19b4dcc
Max <masamune.code@gmail.com>
2019-09-05 17:08:51 +0200

    ADD: basic gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+// // Resources
+// Plugins
+const {
+  parallel,
+  task,
+} = require('gulp');
+
+const git = require('gulp-git');
+
+// // Tasks
+
+// Default tesk, executed when using 'gulp'
+// task('default', parallel('copy-assets'));
